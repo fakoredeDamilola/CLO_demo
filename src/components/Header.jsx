7dc11278124d7ef2f469b1383977c88fedcc7b66
Adesoba Charles <yomiadesoba@gmail.com>
2023-06-17 06:27:28 +0100

    addedstyles, implemented delete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,54 +1,33 @@
 import React from "react";
-// import { Container } from "react-bootstrap";
-import { Navbar } from "react-bootstrap";
-// import Nav from "react-bootstrap/Nav";
-// import ButtonGroup from "react-bootstrap";
-// import Button from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 const Header = () => {
   return (
     <header>
-      <Navbar className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Cutlist Optimizer
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <button className="btn btn-secondary">Calculate</button>
-              </li>
-              <div className="btn-group" role="group">
-                <li className="nav-item">
-                  <button className="btn btn-secondary">Save</button>
-                </li>
-                <li className="nav-item">
-                  <button className="btn btn-secondary">Save as</button>
-                </li>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#">Cutlist Optimizer</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarSupportedContent" />
+
+          <Navbar.Collapse id="navbarSupportedContent">
+            <Nav className="ms-auto align-items-center">
+              <Button className="me-2">Calculate</Button>
+              <ButtonGroup aria-label="save options" className="me-2 my-2">
+                <Button>Save</Button>
+                <Button>Save as</Button>
+              </ButtonGroup>
+              <div className="d-flex flex-column align-items-center">
+                <Button className="mb-2">Settings</Button>
+                <Button className="mb-2">Sign in</Button>
+                <Button className="mb-2">More</Button>
               </div>
-              <li className="nav-item">
-                <button className="btn btn-secondary">settings</button>
-              </li>
-              <li className="nav-item">
-                <button class="btn btn-secondary">Sign in</button>
-              </li>
-              <li className="nav-item">
-                <button className="btn btn-secondary">more</button>
-              </li>
-            </ul>
-          </div>
-        </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </header>
   );
---
+++ src/components/Worksheet.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import Row from "./row";
+import Table from "react-bootstrap/Table";
+
+const Worksheet = () => {
+  const defaultRows = [
+    <Row key={0} onDelete={() => handleDelete(0)} />,
+    <Row key={1} onDelete={() => handleDelete(1)} />,
+    <Row key={2} onDelete={() => handleDelete(2)} />,
+  ];
+  const [rows, setRows] = useState(defaultRows);
+
+  const addRow = () => {
+    setRows([
+      ...rows,
+      <Row key={rows.length} onDelete={() => handleDelete(rows.length)} />,
+    ]);
+  };
+
+  const handleDelete = (index) => {
+    setRows((prevRows) => {
+      const updatedRows = [...prevRows];
+      updatedRows.splice(index, 1);
+      return updatedRows;
+    });
+  };
+
+  useEffect(() => {
+    const defaultRows = 5;
+
+    for (let i = 0; i < defaultRows; i++) {
+      addRow();
+    }
+  }, []);
+
+  return (
+    <div>
+      <Table striped borderless hover variant="dark" size="sm" responsive>
+        <thead>
+          <tr>
+            <th colSpan="5" className="text-capitalize">
+              panels
+            </th>
+          </tr>
+          <tr>
+            <th colSpan="1" className="text-capitalize">
+              length
+            </th>
+            <th colSpan="1" className="text-capitalize">
+              width
+            </th>
+            <th colSpan="1" className="text-capitalize">
+              quantity
+            </th>
+            <th colSpan="1" className="text-capitalize">
+              result
+            </th>
+            <th colSpan="1" className="capitalize"></th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+      <button onClick={addRow}>Add Row</button>
+    </div>
+  );
+};
+
+export default Worksheet;
