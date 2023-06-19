4f0a601bbd646b4a4b85d0dd30c191871518f3a9
fakoredeDamilola <dfakorede432@stu.ui.edu.ng>
2023-06-19 21:50:05 +0100

    change

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ import Table from "./components/Table";
 function App() {
   return (
     <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, cum,
+      aliquam quis quibusdam esse nam, aspernatur molestiae explicabo ducimus
+      ipsa deserunt! Assumenda aperiam ducimus eligendi veniam. Molestiae,
+      blanditiis eum. Fugit veritatis doloribus inventore? Praesentium non
+      similique sit, obcaecati atque officia officiis iste quae nesciunt magni,
+      placeat numquam consequatur eligendi quam?
       <Header />
       <Table />
     </>
